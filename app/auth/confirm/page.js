fc0e54b2e3eb7c624feea95c29c92728abc8b085
Oluyemi Paul <oluola96@gmail.com>
2025-04-01 06:45:47 +0100

    Update page.js

--- app/auth/confirm/page.js
+++ app/auth/confirm/page.js
@@ -1,52 +1,13 @@
-"use client"; // Ensure this is a Client Component
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+import ConfirmEmail from "@/components/EmailConfirmation"
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, Suspense, useState } from "react";
+const ConfirmEmail = dynamic(() => import("./ConfirmEmail"), { ssr: false });
 
-export default function ConfirmEmail() {
-
-  const BASE_API_URL = process.env.NEXT_PUBLIC_API_BASE_URL    
-  const router = useRouter();
-  const searchParams = useSearchParams(); 
-  const confirmation_token = searchParams.get("confirmation_token");
-  const email = searchParams.get("email");
-
-  const [status, setStatus] = useState("Confirming...");
-
-  useEffect(() => {
-    const confirmEmail = async () => {
-      if (!confirmation_token || !email) {
-        setStatus("Invalid confirmation link.");
-        return;
-      }
-
-      try {
-        const response = await fetch(`${BASE_API_URL}/authors/confirmation`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({confirmation_token, email }),
-        });
-
-        if (response.ok) {
-          setStatus("Email confirmed! Redirecting...");
-          setTimeout(() => router.push("/sign_in"), 2000);
-        } else {
-          setStatus("Email confirmation failed.");
-        }
-      } catch (error) {
-        setStatus("An error occurred. Please try again.");
-        console.error("Email confirmation error:", error);
-      }
-    };
-
-    confirmEmail();
-  }, [confirmation_token, email, router]);
-
+export default function ConfirmEmailPage() {
   return (
     <Suspense fallback={<h1>Loading...</h1>}>
-    <div>
-      <h1>{status}</h1>
-    </div>
+      <ConfirmEmail />
     </Suspense>
   );
 }
