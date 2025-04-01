fc609bae960652c6d1f050c0ee5da3c8d5b82ee2
Oluyemi Paul <oluola96@gmail.com>
2025-04-01 06:55:14 +0100

    Update page.js

--- app/auth/confirm/page.js
+++ app/auth/confirm/page.js
@@ -1,10 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { Suspense } from "react";
 import ConfirmEmail from "@/components/EmailConfirmation"
-
-const ConfirmEmail = dynamic(() => import("./ConfirmEmail"), { ssr: false });
 
 export default function ConfirmEmailPage() {
   return (
